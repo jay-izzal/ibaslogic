628e8304fcd65621a5be601e07bbf92020d58373
Jay <jay.izzal@gmail.com>
2022-10-03 20:50:57 +1300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,37 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Jay-Izzal",
+    description: "A professional Wedding DJ",
+    author: "Justin Nehemia",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Metadata from "../components/metadata"
+import Layout from "../components/layout"
+
+const Contact = () => {
+  return (
+    <Layout>
+      <Metadata title="Contact" description="This is my contact page" />
+      <h1>Contact Page</h1>
+      <p>You can find Ibas on Twitter via @ibaslogic</p>
+    </Layout>
+  )
+}
+
+export default Contact
